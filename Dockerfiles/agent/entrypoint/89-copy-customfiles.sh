d779efea881aef276ecd405e4005aa730d898221
Yann Hourdel <yann@hourdel.fr>
2019-02-25 01:12:18 +0100

    Add support for ENV variables inside config files

--- Dockerfiles/agent/entrypoint/89-copy-customfiles.sh
+++ Dockerfiles/agent/entrypoint/89-copy-customfiles.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Copy the custom checks and confs in the /etc/datadog-agent folder
-find /conf.d -name '*.yaml' -exec cp --parents -fv {} /etc/datadog-agent/ \;
+find /conf.d -name '*.yaml' | while read line; do
+  echo "'$line' -> '/etc/datadog-agent$line'"
+  perl -p -e 's/\$\{(\w+)\}/(exists $ENV{$1}?$ENV{$1}:"")/eg' < "$line" > "/etc/datadog-agent$line"
+done
+
 find /checks.d -name '*.py' -exec cp --parents -fv {} /etc/datadog-agent/ \;
